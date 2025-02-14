ed24d1210011cead8592edf3880d148b3fccbf57
SayanBiswas23 <bsayan238@gmail.com>
2025-02-14 20:38:59 +0530

    jokes updated again ffs

--- Backend/index.js
+++ Backend/index.js
@@ -17,11 +17,31 @@ app.get('/login', (req,res)=>{
 //get a list of 5 jokes
 app.get('/jokes', (req, res) => {
     const jokes = [
-        { id: 1, joke: "Why don't scientists trust atoms? Because they make up everything!" },
-        { id: 2, joke: "Why did the scarecrow win an award? Because he was outstanding in his field!" },
-        { id: 3, joke: "Why don't skeletons fight each other? They don't have the guts." },
-        { id: 4, joke: "What do you call fake spaghetti? An impasta!" },
-        { id: 5, joke: "Why did the bicycle fall over? Because it was two-tired!" }
+        { 
+            id: 1, 
+            title: "Chemistry Humor",
+            joke: "Why don't scientists trust atoms? Because they make up everything!" 
+        },
+        { 
+            id: 2, 
+            title: "Farm Jokes",
+            joke: "Why did the scarecrow win an award? Because he was outstanding in his field!" 
+        },
+        { 
+            id: 3, 
+            title: "Spooky Humor",
+            joke: "Why don't skeletons fight each other? They don't have the guts." 
+        },
+        { 
+            id: 4, 
+            title: "Food Puns",
+            joke: "What do you call fake spaghetti? An impasta!" 
+        },
+        { 
+            id: 5, 
+            title: "Transportation Jokes",
+            joke: "Why did the bicycle fall over? Because it was two-tired!" 
+        }
     ];
     res.json(jokes);
 });
