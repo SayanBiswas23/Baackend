1633ed46ee815eccc5d96e4e389ec00ebb0b04f9
SayanBiswas23 <bsayan238@gmail.com>
2025-02-14 20:36:22 +0530

    jokes added

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,19 @@ app.get('/instagram',(req,res) =>{
 app.get('/login', (req,res)=>{
     res.send("<h1>login at this web host</h1>")
 })
+
+//get a list of 5 jokes
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        { id: 1, joke: "Why don't scientists trust atoms? Because they make up everything!" },
+        { id: 2, joke: "Why did the scarecrow win an award? Because he was outstanding in his field!" },
+        { id: 3, joke: "Why don't skeletons fight each other? They don't have the guts." },
+        { id: 4, joke: "What do you call fake spaghetti? An impasta!" },
+        { id: 5, joke: "Why did the bicycle fall over? Because it was two-tired!" }
+    ];
+    res.json(jokes);
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
