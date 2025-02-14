8d6180e5e20dd95e5470b2d391c4322e8e1ca1b4
SayanBiswas23 <bsayan238@gmail.com>
2025-02-14 20:31:04 +0530

    fresh folder struct updated

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config
-const express = require('express')
+// require('dotenv').config()
+import express from 'express'
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,6 +13,6 @@ app.get('/instagram',(req,res) =>{
 app.get('/login', (req,res)=>{
     res.send("<h1>login at this web host</h1>")
 })
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
